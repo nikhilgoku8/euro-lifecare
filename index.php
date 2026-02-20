e4ea5d07c148bc16865cba0f5b86ba9d0831f596
nikhilgoku8 <nikhilgoku8@gmail.com>
2026-02-20 11:13:02 +0530

    Initialization

---
+++ index.php
@@ -0,0 +1,526 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+<title>Html Framework</title>
+<meta name="description" content="," />
+<meta name="keywords" content="" />
+
+<link href="css/reset.css" rel="stylesheet" type="text/css" />
+<link href="css/ace-responsive-menu.css" rel="stylesheet" type="text/css" />
+
+<link href="css/animate.4.1.1.css" rel="stylesheet" type="text/css" />
+
+<!---fonts-->
+<link href="css/fontawesome.7.0.1.min.css" rel="stylesheet" type="text/css" />
+<!-- <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap" rel="stylesheet"> -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
+
+<!-- slider -->
+<link href="plugins/owl-carousel/owl.carousel.min.css" rel="stylesheet">
+<link href="plugins/owl-carousel/owl.theme.default.min.css" rel="stylesheet">
+
+<!-- <link href="plugins/venobox/venobox.min.css" rel="stylesheet" type="text/css" media="screen" /> -->
+
+<link href="plugins/ticker/eocjs-newsticker.css" rel="stylesheet" type="text/css" media="screen" />
+
+
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+<link href="css/responsive.css" rel="stylesheet" type="text/css" />
+
+<!-- <script src="js/jquery.3.7.1.min.js"></script> -->
+<script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+
+<header>  
+
+<div class="top_bar">
+    <div class="contain_90">
+        <div class="inner_container">
+            
+            <div class="left_pane">
+                <a href="#">
+                    <i class="fa-brands fa-facebook-f"></i>
+                </a>
+                <a href="#">
+                    <i class="fa-brands fa-linkedin-in"></i>
+                </a>
+                <a href="#">
+                    <i class="fa-brands fa-instagram"></i>
+                </a>
+            </div>
+            <div class="right_pane">
+                <a href="#" class="ph_num">+998 78 150 15 10</a>
+                <a href="#">info@eurolifecare.uz</a>
+                <span>Mon - Sat: 9.00am - 18.00pm</span>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<div class="lower_bar">
+    <div class="contain_90">
+        <div class="inner_container">
+            <div class="logo">
+                <a href="./"><img src="images/logo.svg" alt="" /></a>
+            </div>
+            <nav>
+                <!-- Menu Toggle btn-->
+                <div class="menu-toggle">
+                    <h3>Menu</h3>
+                    <button type="button" id="menu-btn">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+                <!-- Responsive Menu Structure-->
+                <!--Note: declare the Menu style in the data-menu-style="horizontal" (options: horizontal, vertical, accordion) -->
+                <ul id="respMenu" class="ace-responsive-menu" data-menu-style="horizontal">
+                    <li>
+                        <a href="#">
+                            <span class="title">Home</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="title">About Us</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                            <span class="title">Products</span>
+                            <span class="arrow"></span> 
+                        </a>
+                        <ul>
+                            <li><a href="#">Gastroenterology</a></li>
+                            <li><a href="#">Pediatrician</a></li>
+                            <li><a href="#">Gynecology</a></li>
+                            <li><a href="#">OTC</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="title">Partners</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <span class="title">Contacts</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+            <div class="c2z_btn">
+                <a href="#" class="lt_blue_btn">Book Online</a>
+            </div>
+        </div>
+    </div>
+</div>
+</header>
+<!-- End of Responsive Menu -->
+
+
+<div id="main">
+  
+<div class="hero">
+    <div class="main_banner owl-carousel owl-theme">
+
+        <div class="item_box">
+            <div class="img_box">
+                <img src="https://placehold.co/1920x1080" alt="" title="">
+            </div>
+            <div class="banner_overlay_box">
+                <div class="text_box">
+                    <div class="sub_head">Leading pharmaceutical marketing and distribution in Uzbekistan and the CIS region.</div>
+                    <div class="head">Bridging Global Healthcare with Local Expertise.</div>
+                </div>
+            </div>
+        </div>
+        <div class="item_box">
+            <div class="img_box">
+                <img src="https://placehold.co/1920x1080" alt="" title="">
+            </div>
+            <div class="banner_overlay_box">
+                <div class="text_box">
+                    <div class="sub_head">Empowering healthcare professionals with reliable treatments and evidence-based solutions.</div>
+                    <div class="head">Dedicated to Enhancing Life Through Quality Medicine.</div>
+                </div>
+            </div>
+        </div>
+        <div class="item_box">
+            <div class="img_box">
+                <img src="https://placehold.co/1920x1080" alt="" title="">
+            </div>
+            <div class="banner_overlay_box">
+                <div class="text_box">
+                    <div class="sub_head">Join a network that reaches millions of lives through strategic partnership and scientific integrity.</div>
+                    <div class="head">Shaping the Future of Pharmaceutical Distribution</div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+<!-- hero -->
+
+<div class="info_band">
+    <div class="contain_90">
+        <div class="inner_container">
+            
+            <div class="info_boxes">
+                <div class="info_box">
+                    <div class="icon_box">
+                        <img src="images/icons/years-of-experience.png">
+                    </div>
+                    <div class="text">
+                        <span class="numbers_txt">25+</span>
+                        <span class="info_txt">Years of Experience</span>
+                    </div>
+                </div>
+                <div class="info_box">
+                    <div class="icon_box">
+                        <img src="images/icons/distribution-partners.png">
+                    </div>
+                    <div class="text">
+                        <span class="numbers_txt">500+ </span>
+                        <span class="info_txt">Distribution Partners</span>
+                    </div>
+                </div>
+                <div class="info_box">
+                    <div class="icon_box">
+                        <img src="images/icons/global-markets.png">
+                    </div>
+                    <div class="text">
+                        <span class="numbers_txt">10+</span>
+                        <span class="info_txt"> Global Markets</span>
+                    </div>
+                </div>
+                <div class="info_box">
+                    <div class="icon_box">
+                        <img src="images/icons/specialized-staff.png">
+                    </div>
+                    <div class="text">
+                        <span class="numbers_txt">50+ </span>
+                        <span class="info_txt">Specialized Staff</span>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+<!-- info_band -->
+
+<div class="about_us">
+    <div class="contain_90">
+        <div class="inner_container">
+            
+            <div class="col-sm-6">
+                <div class="img_box">
+                    <img src="images/800x650.png">
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="text_box">
+                    <div class="sub_heading">OUR LEGACY</div>
+                    <div class="heading">Dedicated to Excellence in Pharmaceutical Distribution</div>
+                    <div class="text">
+                        <p>At Euro Lifecare Uzbekistan, we believe that quality healthcare should be accessible to everyone. Since our inception, we have served as a vital bridge between international pharmaceutical innovations and the local medical community.</p>
+                        <p>Our approach combines global logistical standards with a deep understanding of the Uzbekistan market. We don't just distribute medicine; we ensure that every product—from life-saving treatments to daily supplements—meets the highest benchmarks of safety and efficacy.</p>
+                    </div>
+                    <div class="c2a_btn">
+                        <a href="#" class="dk_blue_btn">Know More</a>
+                    </div>
+                </div>
+            </div>
+            <div class="clr"></div>
+
+        </div>
+    </div>
+</div>
+<!-- about_us -->
+
+<div class="ticker_wrapper">
+    <div id="ticker_1">
+        <span>Gastroenterology </span>
+        <span>Pediatrics </span>
+        <span>Gynecology </span>
+        <span>Cardiology </span>
+        <span>OTC Solutions </span>
+        <span>Dietary Supplements </span>
+        <span>Evidence-Based Medicine </span>
+        <span>Quality-Assured Pharmaceuticals </span>
+        <span>Innovative Healthcare Solutions</span>
+    </div>
+</div>
+<!-- ticker_wrapper -->
+
+<div class="products_wrapper">
+    <div class="contain_90">
+        <div class="inner_container">
+            
+            <div class="upper_sec">
+                <div class="left_pane">
+                    <div class="sub_heading">OUR SPECIALITIES</div>
+                    <div class="heading white">Advanced Therapeutic Solutions<br> for Better Health</div>
+                </div>
+                <div class="right_pane">
+                    <div class="c2a_btn">
+                        <a href="#" class="lt_blue_btn dk_blue_font_hover">View All</a>
+                    </div>
+                </div>
+            </div>
+            <!-- upper_sec -->
+            <div class="product_boxes">
+                <div class="product_box">
+                    <div class="icon_box">
+                        <img src="images/icons/gastroenterology.png">
+                    </div>
+                    <div class="product_name">Gastroenterology</div>
+                    <!-- <div class="doctors_count">5 Doctors</div> -->
+                </div>
+                <div class="product_box">
+                    <div class="icon_box">
+                        <img src="images/icons/pediatrician.png">
+                    </div>
+                    <div class="product_name">Pediatrician</div>
+                    <!-- <div class="doctors_count">5 Doctors</div> -->
+                </div>
+                <div class="product_box">
+                    <div class="icon_box">
+                        <img src="images/icons/gynecology.png">
+                    </div>
+                    <div class="product_name">Gynecology</div>
+                    <!-- <div class="doctors_count">5 Doctors</div> -->
+                </div>
+                <div class="product_box">
+                    <div class="icon_box">
+                        <img src="images/icons/otc.png">
+                    </div>
+                    <div class="product_name">OTC</div>
+                    <!-- <div class="doctors_count">5 Doctors</div> -->
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+<!-- products_wrapper -->
+
+<div class="global_partners">
+    <div class="contain_90">
+        <div class="inner_container">
+            
+            <div class="global_partners_slider owl-carousel owl-theme">
+                <div class="item_box">
+                    <img src="images/global-partners/acs-dobfar.jpg">
+                </div>
+                <div class="item_box">
+                    <img src="images/global-partners/bells-healthcare.jpg">
+                </div>
+                <div class="item_box">
+                    <img src="images/global-partners/dr.med.-aufdermaur-ag.jpg">
+                </div>
+                <div class="item_box">
+                    <img src="images/global-partners/eurodrug.jpg">
+                </div>
+                <div class="item_box">
+                    <img src="images/global-partners/genepharm.jpg">
+                </div>
+                <div class="item_box">
+                    <img src="images/global-partners/ntcc.jpg">
+                </div>
+                <div class="item_box">
+                    <img src="images/global-partners/organosyn.png">
+                </div>
+                <div class="item_box">
+                    <img src="images/global-partners/vorwards-pharma-logo.png">
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+</div>
+<!-- main -->
+
+<footer>
+    <div class="upper_sec">
+        <div class="contain_90">
+            <div class="inner_container">
+                
+                <div class="info_box">
+                    <div class="logo_box">
+                        <a href="#">
+                            <img src="images/footer-logo.png">
+                        </a>
+                    </div>
+                    <div class="company_info">
+                        Our team of highly skilled medical professionals is committed to delivering compassionate and expert care across various specialties.
+                    </div>
+                    <div class="c2a_btn">
+                        <a href="#" class="lt_blue_btn dk_blue_font_hover">Make an Appointment</a>
+                    </div>
+                </div>
+                <div class="info_box">
+                    <div class="title">Contact Us</div>
+                    <div class="address">We are Here: Barakat Business Center,<br> 2 Afrosiyob, Tashkent, 100031</div>
+                    <div class="email_txt">Send Mail: <a href="#">example@test.com</a> </div>
+                    <div class="contact_txt">Call Us:  <a href="#">+1 800 432 45 34</a> </div>
+                    <ul class="social_links">
+                        <li>
+                            <a href="#">
+                                <i class="fa-brands fa-facebook-f"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="fa-brands fa-linkedin-in"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="fa-brands fa-instagram"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="info_box">
+                    <div class="our_dept">
+                        <div class="title">Our Products</div>
+                        <ul>
+                            <li><a href="#">Gastroenterology</a></li>
+                            <li><a href="#">Pediatrician</a></li>
+                            <li><a href="#">Gynecology</a></li>
+                            <li><a href="#">OTC</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <div class="lower_sec">
+        <div class="contain_90">
+            <div class="inner_container">
+                
+                <div class="left_pane">© 2025 Euro Lifecare Ltd. All Rights Reserved.</div>
+                <div class="right_pane">
+                    <a href="#">Terms of Use</a>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+</footer>
+
+<!--sticky header-->
+<!-- <script src="js/classie.js"></script>
+<script>
+function init() {
+window.addEventListener('scroll', function(e){
+  var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+  shrinkOn = 50,
+  header = document.querySelector("header");
+  if (distanceY > shrinkOn) {
+  classie.add(header,"smaller");
+  } else {
+  if (classie.has(header,"smaller")) {
+    classie.remove(header,"smaller");
+  }
+  }
+});
+}
+window.onload = init();
+</script> -->
+<script src="js/common.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+
+
+<script src="js/ace-responsive-menu.js"></script>
+<script>
+$(document).ready(function () {
+    $("#respMenu").aceResponsiveMenu({
+        resizeWidth: '768', // Set the same in Media query       
+        animationSpeed: 'fast', //slow, medium, fast
+        accoridonExpAll: false //Expands all the accordion menu on click
+    });
+});
+</script>
+
+<!-- <script src="plugins/venobox/venobox.min.js"></script>
+<script>
+$(document).ready(function(){
+    new VenoBox({
+        selector: '.new_veno',
+        // numeration: true,
+        // infinigall: true,
+        // share: true,
+        // spinner: 'rotating-plane'
+    });
+});
+</script> -->
+
+<script src="js/wow.min.js"></script>
+<script>
+new WOW().init();
+</script>
+
+<!--slider-->
+<script src="plugins/owl-carousel/owl.carousel.js"></script>
+<script src="plugins/owl-carousel/owl-content-animation.js"></script>
+
+<script src="plugins/ticker/eocjs-newsticker.js"></script>
+
+
+<script>
+
+$('#ticker_1').eocjsNewsticker({
+    divider:'',
+  speed: 25
+});
+
+// $('#example-2').eocjsNewsticker({
+//   divider:    '***',
+//   type:       'ajax',
+//   source:     './example.json',
+//   interval:   30
+// });
+
+// $('#example-3').eocjsNewsticker({
+//   speed:      10,
+//   divider:    '+',
+//   type:       'ajax',
+//   source:     './example.json',
+//   fetch:      true,
+//   interval:   30
+// });
+
+// $('#example-4').eocjsNewsticker({
+//   speed:      25,
+//   divider:    '***',
+//   direction:  'rtl'
+// });
+
+// $('#example-5').eocjsNewsticker({
+//   divider:    '***',
+//   type:       'ajax',
+//   source:     './example.json',
+//   interval:   30,
+//   direction:  'rtl'
+// });
+
+</script>
+
+</body>
+</html>
